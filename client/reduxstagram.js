d167010f6562754af8197c00b5829bb60df5182e
Francisco De La Cruz <helloquico@gmail.com>
2016-05-23 20:54:32 -0400

    Access Dispatch and State

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,19 +5,19 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // import Components
-import Main from './components/Main';
-import Single from './components/Single';
-import PhotoGrid from './components/PhotoGrid';
+  import App from './components/App';
+  import Single from './components/Single';
+  import PhotoGrid from './components/PhotoGrid';
 
-// import react router deps
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
-import store, { history } from './store';
+  // import react router deps
+  import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+  import { Provider } from 'react-redux';
+  import store, { history } from './store';
 
-const router = (
-    <Provider store={store}>
+  const router = (
+      <Provider store={store}>
       <Router history={history}>
-        <Route path="/" component={Main}>
+        <Route path="/" component={App}>
           <IndexRoute component={PhotoGrid}></IndexRoute>
           <Route path="/view/:postId" component={Single}></Route>
         </Route>
